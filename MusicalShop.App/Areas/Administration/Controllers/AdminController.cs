40a727686bb8a59ac87af3823f9cb6eac26c1c3d
Denis Tabutov <125385668+denjiyy@users.noreply.github.com>
2024-12-18 22:58:44 +0200

    Add files via upload

---
+++ MusicalShop.App/Areas/Administration/Controllers/AdminController.cs
@@ -0,0 +1,11 @@
+﻿namespace MusicalShop.App.Areas.Administration.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize(Roles = "Admin")]
+    [Area("Administration")]
+    public class AdminController : Controller
+    {
+    }
+}
---
+++ MusicalShop.App/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,211 @@
+﻿namespace MusicalShop.App.Areas.Identity.Pages.Account
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.AspNetCore.WebUtilities;
+    using Microsoft.Extensions.Logging;
+    using MusicalShop.Data.Models;
+    using MusicalShop.Services;
+
+    [AllowAnonymous]
+    public class RegisterModel : PageModel
+    {
+        private readonly SignInManager<MusicalShopUser> _signInManager;
+        private readonly UserManager<MusicalShopUser> _userManager;
+        private readonly ILogger<RegisterModel> _logger;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        /*private readonly IEmailSender _emailSender;*/
+
+        public RegisterModel(
+            UserManager<MusicalShopUser> userManager,
+            SignInManager<MusicalShopUser> signInManager,
+            ILogger<RegisterModel> logger,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+            _roleManager = roleManager;
+
+            /*_emailSender = emailSender;*/
+
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            public string Username { get; set; }
+
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+
+            [Required]
+            public string FirstName { get; set; }
+
+            [Required]
+            public string LastName { get; set; }
+            [Required]
+            public DateTime BirthDate{get;set;}
+        }
+
+        public async Task OnGetAsync(string returnUrl = null)
+        {
+            ReturnUrl = returnUrl;
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+        }
+
+        // public async Task<IActionResult> LoginWithFacebookAsync(string accessToken)
+        // {
+        //     var validatedTokenResult = await facebookAuthService.ValidateAccessTokenAsync(accessToken);
+        //
+        //     if (validatedTokenResult.Data.IsValid)
+        //     {
+        //         /*return new AuthenticationResult
+        //         {
+        //             Errors = new[] { "Invalid Facebook token" }
+        //         };*/
+        //     }
+        //     var userInfo = await facebookAuthService.GetUserInfoAsync(accessToken);
+        //     var userFromFacebook = await _userManager.FindByEmailAsync(userInfo.Email);
+        //
+        //     if (userFromFacebook == null)
+        //     {
+        //         var user = new MusicalShopUser
+        //         {
+        //             Email = userFromFacebook.Email,
+        //             UserName = userFromFacebook.UserName,
+        //
+        //         };
+        //
+        //         var createdResult = await _userManager.CreateAsync(user);
+        //         await _userManager.AddToRoleAsync(user, "User");
+        //
+        //         if (!createdResult.Succeeded)
+        //         {
+        //
+        //             //Make it right
+        //             /*return new AuthenticationResult
+        //             {
+        //             Errors = new[] { "Something went wrong" }
+        //             };*/
+        //         }
+        //         
+        //         await _signInManager.SignInAsync(user, isPersistent: false);
+        //     }
+        //     this.Redirect("/Identity/Account/Register");
+        // }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl = "/";
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
+            if (ModelState.IsValid)
+            {
+                var user = new MusicalShopUser
+                {
+                    UserName = Input.Username,
+                    Email = Input.Email,
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName,
+                    BirthDate = Input.BirthDate
+                };
+
+                var result = await _userManager.CreateAsync(user, Input.Password);
+
+                if (!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        _logger.LogError($"Error Code: {error.Code}, Description: {error.Description}");
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    return Page();
+                }
+
+                if (result.Succeeded)
+                {
+                    // Ensure the "User" role exists before assigning it
+                    await EnsureRoleExists("User");
+                    await _userManager.AddToRoleAsync(user, "User");
+
+                    // Generate email confirmation token
+                    var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    if (!string.IsNullOrEmpty(token))
+                    {
+                        var confirmationLink = Url.Action("ConfirmEmail", "Account",
+                            new { userId = user.Id, token = token }, Request.Scheme);
+
+                        _logger.Log(LogLevel.Warning, confirmationLink);
+                    }
+
+                    _logger.LogInformation("User created a new account with password.");
+
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    var callbackUrl = Url.Page(
+                        "/Account/ConfirmEmail",
+                        pageHandler: null,
+                        values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
+                        protocol: Request.Scheme);
+
+                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    {
+                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                    }
+                    else
+                    {
+                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        return LocalRedirect(returnUrl);
+                    }
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            // If we got this far, something failed, redisplay the form
+            return Page();
+        }
+
+        private async Task EnsureRoleExists(string roleName)
+        {
+            if (!await _roleManager.RoleExistsAsync(roleName))
+            {
+                var role = new IdentityRole { Name = roleName, NormalizedName = roleName.ToUpper() };
+                await _roleManager.CreateAsync(role);
+            }
+        }
+    }
+}
