3135e1ac9d4260cbed0f8d896ced75a9a0ddf835
Denis Tabutov <125385668+denjiyy@users.noreply.github.com>
2025-01-07 16:57:44 +0200

    Delete MusicalShop.Services directory

--- MusicalShop.Services/IOrderService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MusicalShop.Services
-{
-    using MusicalShop.Services.Models;
-    using System.Threading.Tasks;
-    public interface IOrderService
-    {
-        Task<bool> Create(OrderServiceModel model);
-    }
-}
