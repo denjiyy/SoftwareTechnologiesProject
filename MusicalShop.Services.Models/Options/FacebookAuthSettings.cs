1f28ffb4d0313ae2c89ff2a79a1820d1bb8508b5
Denis Tabutov <125385668+denjiyy@users.noreply.github.com>
2025-01-07 16:57:25 +0200

    Delete MusicalShop.Services.Models directory

--- MusicalShop.Services.Models/Options/FacebookAuthSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MusicalShop.Services.Models.Options
-{
-    public class FacebookAuthSettings
-    {
-        public string AppId { get; set; }
-        public string AppSecret { get; set; }
-    }
-}
